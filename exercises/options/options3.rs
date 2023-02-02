91da7d63ebea390da41b0656b917d87c6258fbb1
Yuki Soma <somay@users.noreply.github.com>
2023-02-02 13:03:11 +0900

    solve 3

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -1,8 +1,6 @@
 // options3.rs
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -12,7 +10,9 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        // https://doc.rust-lang.org/std/keyword.ref.html
+        // > ref annotates pattern bindings to make them borrow rather than move.
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
     }
     y; // Fix without deleting this line.
